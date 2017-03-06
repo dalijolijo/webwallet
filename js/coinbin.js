2845edc628db6618c71e672f0c3690b59b274fb9
HashEngineering <hashengineeringsolutions@gmail.com>
2017-03-05 16:04:03 -0800

    Fix reverse hash error with unspent

--- js/coinbin.js
+++ js/coinbin.js
@@ -63,7 +63,7 @@ $(document).ready(function() {
 		$("#openWallet").addClass("hidden").show();
 
 		$("#walletAddress").html("");
-		$("#walletHistory").attr('href','https://btc.blockr.io/address/info/');
+		$("#walletHistory").attr('href','https://chainz.cryptoid.info/grs/address.dws?');
 
 		$("#walletQrCode").html("");
 		var qrcode = new QRCode("walletQrCode");
@@ -795,7 +795,7 @@ $(document).ready(function() {
 		var tx = coinjs.transaction();
 		tx.listUnspent(redeem.addr, function(data){
 			if(redeem.addr) {
-				$("#redeemFromAddress").removeClass('hidden').html('<span class="glyphicon glyphicon-info-sign"></span> Retrieved unspent inputs from address <a href="https://btc.blockr.io/address/info/'+redeem.addr+'" target="_blank">'+redeem.addr+'</a>');
+				$("#redeemFromAddress").removeClass('hidden').html('<span class="glyphicon glyphicon-info-sign"></span> Retrieved unspent inputs from address <a href="https://chainz.cryptoid.info/grs/address.dws?'+redeem.addr+'" target="_blank">'+redeem.addr+'</a>');
 
 				$.each($(data).find("unspent").children(), function(i,o){
 					var tx = $(o).find("tx_hash").text();
@@ -941,11 +941,11 @@ $(document).ready(function() {
                 console.log(data);
 				if((data.unspent_outputs)){
 					$("#redeemFromAddress").removeClass('hidden').html(
-						'<span class="glyphicon glyphicon-info-sign"></span> Retrieved unspent inputs from address <a href="https://btc.blockr.io/address/info/'+
+						'<span class="glyphicon glyphicon-info-sign"></span> Retrieved unspent inputs from address <a href="https://chainz.cryptoid.info/grs/address.dws?'+
 						redeem.addr+'" target="_blank">'+redeem.addr+'</a>');
 					for(i = 0; i < data.unspent_outputs.length; ++i){
 						var o = data.unspent_outputs[i];
-						var tx = o.tx_hash;//((""+o.tx_hash).match(/.{1,2}/g).reverse()).join("")+'';
+						var tx = ((""+o.tx_hash).match(/.{1,2}/g).reverse()).join("")+'';
 						if(tx.match(/^[a-f0-9]+$/)){
 							var n = o.tx_ouput_n;
 							var script = (redeem.isMultisig==true) ? $("#redeemFrom").val() : o.script;
@@ -975,10 +975,10 @@ $(document).ready(function() {
 			},
 			success: function(data) {
 				if(data.length > 0){
-					$("#redeemFromAddress").removeClass('hidden').html('<span class="glyphicon glyphicon-info-sign"></span> Retrieved unspent inputs from address <a href="https://btc.blockr.io/address/info/'+redeem.addr+'" target="_blank">'+redeem.addr+'</a>');
+					$("#redeemFromAddress").removeClass('hidden').html('<span class="glyphicon glyphicon-info-sign"></span> Retrieved unspent inputs from address <a href="https://chainz.cryptoid.info/grs/address.dws?'+redeem.addr+'" target="_blank">'+redeem.addr+'</a>');
 					for(var i in data){
 						var o = data[i];
-						var tx = o.txid;
+						var tx = ((o.txid).match(/.{1,2}/g).reverse()).join("")+'';
 						var n = o.vout;
 						var script = (redeem.isMultisig==true) ? $("#redeemFrom").val() : o.scriptPubKey;
 						var amount = (o.value /100000000).toFixed(8);
